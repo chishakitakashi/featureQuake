9621457a00c246b0d3c3a36db24abebf9b47d90d
chishakitakashi <e7axu4shai@icloud.com>
2022-06-03 20:21:23 +0900

    動く

--- main.js
+++ main.js
@@ -1,100 +1,52 @@
 const display = document.getElementById("display");
+const choice1 = document.getElementById("choice-1");
+const choice2 = document.getElementById("choice-2");
+const choice3 = document.getElementById("choice-3");
+const choice4 = document.getElementById("choice-4");
 const idoElement = document.getElementById("ido");
 const keidoElement = document.getElementById("keido");
 const addButton = document.getElementById("add-button");
-let ganreSelect = document.getElementById("ganre");
-
+// ​地震のコードと説明
+const choices = [
+  {
+    value: "T30_I45_PS",
+    text: "今後30年間で震度5弱以上となる確率",
+  },
+  {
+    value: "T30_I50_PS",
+    text: "今後30年間で震度5強以上となる確率",
+  },
+  {
+    value: "T30_I55_PS",
+    text: "今後30年間で震度6弱以上となる確率",
+  },
+  {
+    value: "T30_I60_PS",
+    text: "今後30年間で震度6強以上となる確率",
+  },
+];
+// クリックしたら4回分の地震情報を出す。
 addButton.onclick = function () {
-  // console.log(`${idoElement.value}`);
-  // console.log(`${keidoElement.value}`);
-  console.log("選択されているのは " + ganreSelect.value + " です");
-  // url = `https://www.j-shis.bosai.go.jp/map/api/pshm/Y2010/AVR/TTL_MTTL/meshinfo.geojson?position=${idoElement.value},${keidoElement.value}&epsg=4612&attr=T30_I45_PS`;
-  url = `https://www.j-shis.bosai.go.jp/map/api/pshm/Y2010/AVR/TTL_MTTL/meshinfo.geojson?position=${idoElement.value},${keidoElement.value}&epsg=4612&attr=${ganreSelect.value}`;
-  console.log(`${url}`);
-
-  // fetch(`${url}`)
-  //   .then((res) => {
-  //     return res.json(); // 結果を json として読み込む
-  //   })
-  //   .then((data) => {
-  //     display.textContent = data.features[0].properties.T30_I45_PS;
-  //     console.log(`${display.textContent}`);
-  //   });
-
-  // 入力欄を空にする
+  for (let i = 0; i < choices.length; i++) {
+    url = "";
+    // 入力された緯度経度をURLに代入
+    url = `https://www.j-shis.bosai.go.jp/map/api/pshm/Y2010/AVR/TTL_MTTL/meshinfo.geojson?position=${idoElement.value},${keidoElement.value}&epsg=4301&attr=${choices[i].value}`;
+    // 配列に記述してあるvalueをURLに足してfetchに使用するURLが完成
+    // url = url + choices[i].value;
+    // 確認用↓
+    console.log(`${url}`);
+    // console.log(choices[i].value);
+    // console.log(choices[i].text);
+    fetch(`${url}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(choices[i].value);
+        display.textContent = data.features[0].properties[choices[i].value];
+        console.log(display.textContent);
+      });
+  }
   keidoElement.value = "";
   idoElement.value = "";
-  // ganreSelect.options[2].selected = true;
-
-  if (ganreSelect.value == "T30_I45_PS") {
-    fetch(`${url}`)
-      .then((res) => {
-        return res.json(); // 結果を json として読み込む
-      })
-      .then((data) => {
-        display.textContent = data.features[0].properties.T30_I45_PS;
-        console.log("30年間で震度5弱以上となる確率");
-        console.log(`${display.textContent}`);
-      });
-    // 入力欄を空にする
-    keidoElement.value = "";
-    idoElement.value = "";
-    // ganreSelect.options[2].selected = true;
-  } else if (ganreSelect.value == "T30_I50_PS") {
-    fetch(`${url}`)
-      .then((res) => {
-        return res.json(); // 結果を json として読み込む
-      })
-      .then((data) => {
-        display.textContent = data.features[0].properties.T30_I50_PS;
-        console.log("30年間で震度5強以上となる確率");
-        console.log(`${display.textContent}`);
-      });
-
-    // 入力欄を空にする
-    keidoElement.value = "";
-    idoElement.value = "";
-    // ganreSelect.options[2].selected = true;
-  } else if (ganreSelect.value == "T30_I55_PS") {
-    fetch(`${url}`)
-      .then((res) => {
-        return res.json(); // 結果を json として読み込む
-      })
-      .then((data) => {
-        display.textContent = data.features[0].properties.T30_I55_PS;
-        console.log("30年間で震度6弱以上となる確率");
-        console.log(`${display.textContent}`);
-      });
-
-    // 入力欄を空にする
-    keidoElement.value = "";
-    idoElement.value = "";
-    ganreSelect.options[2].selected = true;
-  } else if (ganreSelect.value == "T30_I60_PS") {
-    fetch(`${url}`)
-      .then((res) => {
-        return res.json(); // 結果を json として読み込む
-      })
-      .then((data) => {
-        display.textContent = data.features[0].properties.T30_I60_PS;
-        console.log("30年間で震度6強以上となる確率");
-        console.log(`${display.textContent}`);
-      });
-
-    // 入力欄を空にする
-    keidoElement.value = "";
-    idoElement.value = "";
-    // ganreSelect.options[2].selected = true;
-  } else if (ganreSelect.value == "NoSelect") {
-    display.textContent = "地震の種類を選択してください";
-    console.log("地震の種類を選択してください");
-
-    // 入力欄を空にする
-    keidoElement.value = "";
-    idoElement.value = "";
-    // ganreSelect.options[2].selected = true;
-  } else {
-    console.log("該当なし");
-  }
-  // ganreSelect.options[2].selected = true;
 };
